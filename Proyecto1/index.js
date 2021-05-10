e1c37edeb210349fceaf5a3790953f8a746aed0c
Jose Piñero <jpineroberbel@gmail.com>
2021-05-10 09:05:24 +0200

    Add files via upload

--- Proyecto1/index.js
+++ Proyecto1/index.js
@@ -1,6 +1,6 @@
 
 
-    urlApi = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=6db6a643cb304edda455c178f88113fd"
+    urlApi = "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=bitcoin&apiKey=6db6a643cb304edda455c178f88113fd"
     
 
     function loadDoc() {
