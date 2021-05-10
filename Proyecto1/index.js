b248f775b7457ddf2c5e2155141294771d97650a
Jose Piñero <jpineroberbel@gmail.com>
2021-05-10 09:00:22 +0200

    Add files via upload

---
+++ Proyecto1/index.js
@@ -0,0 +1,21 @@
+
+
+    urlApi = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=6db6a643cb304edda455c178f88113fd"
+    
+
+    function loadDoc() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+
+            respuesta = this.responseText;
+            console.log(respuesta);
+          }
+
+    
+    
+        };
+        xhttp.open("GET", urlApi, true);
+        xhttp.send();
+      }
+ 
